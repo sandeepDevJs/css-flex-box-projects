12c3d63ce86ac76144e41fd0f9ef5a865995600c
Sandeep Gupta <sandeepguptabc@gmail.com>
2022-07-26 23:12:30 +0530

    autoprefixer

---
+++ autoprefixer/gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require("gulp");
+var autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("style", function (e) {
+    return gulp.src("css/styles.css")
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("build"))
+})
+
+gulp.task("watch", function name(params) {
+    gulp.watch("css/styles.css", ["style"]);
+})
